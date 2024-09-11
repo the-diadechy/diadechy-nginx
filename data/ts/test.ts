7a6d2d0143ed831304724b794a90fe6bc89c7d86
Henonaho <futa_henona@protonmail.com>
2024-09-11 15:23:58 -0700

    dynamically add list elements

--- data/ts/test.ts
+++ data/ts/test.ts
@@ -1,14 +1,46 @@
+
 const myRequest = new Request("../words.json");
-const array: string[] = [];
+let array: string[] = [];
+
 fetch(myRequest)
-  .then((response) => response.json())
-  .then((data) => {
-    for (const word of data.words){
+    .then((response) => response.json())
+    .then((data) => {
+    
+    console.log("when is fetch called\n" + data.words);
+    array = data.words;
+    })
+    .then(function(){createList()});
+
+
+//var list = document.getElementById("#words_list");
+//console.log("list: " + list);
+//entry.append(document.createTextNode("gwen"));
+function createList(){
+    
+    console.log("the array: " + array);     
+    console.log("is this being called");
+    var list = $("#words_list");
+    for (const word of array){
+        console.log("word: " + word);
+         $("#words_list").append($("<li>").text(word));
+
+    }
+}
+
+/*
+    function getWords(obj){
+    for (const word of obj.words){
     array.push(word);
-    };
-  });
+    }
+};
+*/
 
-
+/*
+ * get list element
+ * create an li element to append to
+ * append text to list element
+ * append li element to list
+*/
 console.log("test");
  "use strict";
 console.log("test2");
@@ -18,6 +50,7 @@ console.log("test2");
   });
 $("#Tennyson").on("click",function () {
     console.log("clicked");
-    alert(array);
+    createList();
+    // alert(array);
 });
 
